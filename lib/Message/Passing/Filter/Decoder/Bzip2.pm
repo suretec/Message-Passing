c963714eef82f16d8911d5a412dfb0b3e81dee12
Tomas Doran <bobtfish@bobtfish.net>
2012-07-11 19:12:53 +0100

    More docs

--- lib/Message/Passing/Filter/Decoder/Bzip2.pm
+++ lib/Message/Passing/Filter/Decoder/Bzip2.pm
@@ -13,3 +13,37 @@ sub filter {
 __PACKAGE__->meta->make_immutable;
 1;
 
+=head1 NAME
+
+Message::Passing::Decoder::Bzip2 - Use Compress:Bzip2 to encrypt messages
+
+
+=head1 SYNOPSIS
+
+    message-pass --input STDIN --decoder Bzip2 \
+        --output ZeroMQ --output_options '...'
+
+=head1 DESCRIPTION
+
+Uncompresses messages with Compress::Bzip2.
+
+=head1 METHODS
+
+=head2 filter
+
+Uncompresses the message
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Message::Passing::Role::Filter>
+
+=back
+
+=head1 AUTHOR, COPYRIGHT & LICENSE
+
+See L<Message::Passing>.
+
+=cut
+
--- lib/Message/Passing/Filter/Decoder/Gzip.pm
+++ lib/Message/Passing/Filter/Decoder/Gzip.pm
@@ -13,3 +13,30 @@ sub filter {
 __PACKAGE__->meta->make_immutable;
 1;
 
+=head1 NAME
+
+Message::Passing::Filter::Decoder::Gzip - Decompresses messages with Compress::Zlib
+
+=head1 SYNOPSIS
+
+    message-pass --input STDIN --decoder Gzip \
+        --output ZeroMQ --output_options '...'
+
+=head1 DESCRIPTION
+
+Decompresses messages with Compress::Zlib.
+
+=head1 METHODS
+
+=head2 filter
+
+Decompresses the message
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Message::Passing::Role::Filter>
+
+=back
+
