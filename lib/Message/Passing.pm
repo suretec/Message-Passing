90097d869383858289ecb7499c7c61b10064e4a5
Tomas Doran <bobtfish@bobtfish.net>
2013-04-25 23:45:39 +0100

    Version 0.112

--- lib/Message/Passing.pm
+++ lib/Message/Passing.pm
@@ -8,7 +8,7 @@ use MooX::Options flavour => [qw( pass_through )], protect_argv => 0;
 use namespace::clean -except => [qw/ meta new_with_options parse_options _options_data _options_config/];
 use 5.008004;
 
-our $VERSION = '0.111';
+our $VERSION = '0.112';
 $VERSION = eval $VERSION;
 
 around 'parse_options' => sub {
